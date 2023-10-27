d3d37f331f3b50447c29fb7589560f88fd5521dd
jabanlokobann <medboutallaka1@gmail.com>
2023-10-26 19:45:35 -0700

    reverse

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,6 +21,32 @@ char *move_past_star(char *s2)
  *
  * Return: 1 if identical, 0 if false
  */
+int wildcmp(char *s1, char *s2)
+{
+        int ret = 0;
+
+        if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+                return (1);
+        if (*s1 == *s2)
+        {
+                if (!*s1)
+                        return (1);
+                return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+        }
+        if (!*s1 || !s2)
+                return (0);
+        if (*s2 == '*')
+        {
+                s2 = move_past_star(s2);
+                if (!*s2)
+                        return (1);
+                if (*s1 == *s2)
+                        ret += wildcmp(s1 + 1, s2 + 1);
+                ret += inception(s1, s2);
+                return (!!ret);
+        }
+        return (0);
+}
 int inception(char *s1, char *s2)
 {
 	int ret = 0;
@@ -31,38 +57,4 @@ int inception(char *s1, char *s2)
 		ret += wildcmp(s1 + 1, s2 + 1);
 	ret += inception(s1 + 1, s2);
 	return (ret);
-}
-
-/**
- * wildcmp - compares two strings lexicographically
- * @s1: the first string
- * @s2: the second string, can contain wildcard
- *
- * Return: 1 if identical, 0 if false
- */
-int wildcmp(char *s1, char *s2)
-{
-	int ret = 0;
-
-	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
-		return (1);
-	if (*s1 == *s2)
-	{
-		if (!*s1)
-			return (1);
-		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
-	}
-	if (!*s1 || !s2)
-		return (0);
-	if (*s2 == '*')
-	{
-		s2 = move_past_star(s2);
-		if (!*s2)
-			return (1);
-		if (*s1 == *s2)
-			ret += wildcmp(s1 + 1, s2 + 1);
-		ret += inception(s1, s2);
-		return (!!ret);
-	}
-	return (0);
 }
